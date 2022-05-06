57a0585df956aba5f0b3618ae331db508d977dfc
Jonny Ortiz <j0nny.0rt1z@yahoo.com>
2022-05-06 16:12:21 -0400

    Completed assignment

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -1,26 +1,63 @@
-import React, { useState } from "react";
-import { spicyFoods, getNewSpicyFood } from "../data";
+import React, { useState } from 'react';
+import { spicyFoods, getNewSpicyFood } from '../data';
 
 function SpicyFoodList() {
-  const [foods, setFoods] = useState(spicyFoods);
+   const [foods, setFoods] = useState(spicyFoods);
+   const [filterBy, setFilterBy] = useState('All');
 
-  function handleAddFood() {
-    const newFood = getNewSpicyFood();
-    console.log(newFood);
-  }
+   function handleAddFood() {
+      const newFood = getNewSpicyFood();
+      // console.log(newFood);
+      setFoods((foods) => [...foods, newFood]); // my solution
+      /* Canvas solution
+     const newFoodArray = [...foods, newFood];
+     setFoods(newFoodArray);
+     */
+   }
 
-  const foodList = foods.map((food) => (
-    <li key={food.id}>
-      {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
-    </li>
-  ));
+   function deleteFood(foodId) {
+      setFoods(foods.filter((food) => food.id !== foodId));
+   }
 
-  return (
-    <div>
-      <button onClick={handleAddFood}>Add New Food</button>
-      <ul>{foodList}</ul>
-    </div>
-  );
+   function handleFood(foodId, heat) {
+      const updatedFoods = foods.map((food) =>
+         food.id === foodId ? { ...food, heatLevel: heat + 1 } : food
+      );
+      setFoods(updatedFoods);
+   }
+
+   const filteredFoods = foods.filter((food) =>
+      filterBy === 'All' ? true : food.cuisine === filterBy
+   );
+
+   function handleFoodType(e) {
+      setFilterBy(e.target.value);
+   }
+
+   const foodList = filteredFoods.map((food) => (
+      <li key={food.id}>
+         <span onClick={() => handleFood(food.id, food.heatLevel)}>
+            {' '}
+            {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}{' '}
+         </span>
+         &nbsp;
+         <span onClick={() => deleteFood(food.id)}>🚫</span>
+      </li>
+   ));
+
+   return (
+      <div>
+         <button onClick={handleAddFood}>Add New Food</button> &nbsp; &nbsp;
+         <select name="filter" onChange={(e) => handleFoodType(e)}>
+            <option value="All">All</option>
+            <option value="American">American</option>
+            <option value="Sichuan">Sichuan</option>
+            <option value="Thai">Thai</option>
+            <option value="Mexican">Mexican</option>
+         </select>
+         <ul>{foodList}</ul>
+      </div>
+   );
 }
 
 export default SpicyFoodList;
